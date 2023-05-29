a85de2ae1ca68074f322aa8ae5d26191976c0526
PhilipKlatte <klattephilip@gmail.com>
2023-05-29 22:17:38 +0200

    implement barrel.move()

--- scripts/barrel.js
+++ scripts/barrel.js
@@ -16,8 +16,19 @@ class Barrel extends GameObject {
             this.velocityLeft = player.velocityLeft;
             this.velocityUp = player.velocityUp;
             this.velocityDown = player.velocityDown;
-
-
         }
+
+        if (collidesWithOneOf(new Barrel(this.src, this.ctx, this.posX + this.velocityRight - this.velocityLeft, this.posY), walls) === null) {
+            this.posX = this.posX + this.velocityRight - this.velocityLeft;
+        }
+
+        if (collidesWithOneOf(new Barrel(this.src, this.ctx, this.posX, this.posY - this.velocityUp + this.velocityDown), walls) === null) {
+            this.posY = this.posY - this.velocityUp + this.velocityDown;
+        }
+
+        if (this.velocityRight > 0) this.velocityRight -= 0.1 * playerMovementSpeed;
+        if (this.velocityLeft > 0) this.velocityLeft -= 0.1 * playerMovementSpeed;
+        if (this.velocityUp > 0) this.velocityUp -= 0.1 * playerMovementSpeed;
+        if (this.velocityDown > 0) this.velocityDown -= 0.1 * playerMovementSpeed;
     }
 }
