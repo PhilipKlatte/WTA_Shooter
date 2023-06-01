546c5554f07d6fb5a5d568a998b0bfaeb3fe3a3d
Philip <klattephilip@gmail.com>
2023-06-01 09:33:17 +0200

    implement showRectangularCollideZone()

---
+++ scripts/circularCollideZone.js
@@ -0,0 +1,8 @@
+class CircularCollideZone {
+    constructor(fromX, fromY, untilX, untilY) {
+        this.fromX = fromX;
+        this.fromY = fromY;
+        this.untilX = untilX;
+        this.untilY = untilY;
+    }
+}
