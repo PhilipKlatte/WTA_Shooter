83458c3198842fc3efad30779d2f154c3121e7ca
Moritz Hegmann <hegmann.moritz@gmail.com>
2023-06-15 16:28:03 +0200

    Changes by klatt

--- scripts/collisionDetection.js
+++ scripts/collisionDetection.js
@@ -8,13 +8,23 @@ class CollisionDetection{
         if (gameObjectZone instanceof RectangularZone
             && collideObjectZone instanceof RectangularZone){
 
-            return this.#rectangularCZcollidesWithRectangularCZ(gameObject, collideObject);
+            return this.#rectangularCZcollidesWithRectangularCZ(gameObject, gameObjectZone, collideObject, collideObjectZone);
         }
-        if (gameObject.collideZone instanceof CircularCollideZone
-            && collideObject.collideZone instanceof RectangularZone){
+        if (gameObjectZone instanceof CircularZone
+            && collideObjectZone instanceof RectangularZone){
 
-            return this.#circularCZcollidesWithRectangularCZ(gameObject, collideObject);
+            return this.#circularCZcollidesWithRectangularCZ(gameObject, gameObjectZone, collideObject, collideObjectZone);
         }
+    }
+    
+    static #getZone(gameObject, zoneType){
+        let zone = null;
+        
+        gameObject.zones.forEach(elem => {
+            if (elem instanceof zoneType) zone = zoneType;
+        });
+        
+        return zone;
     }
 
     static collidesWithOneOf(gameObject, collideObjects){
@@ -34,39 +44,39 @@ class CollisionDetection{
             if (collideObject === undefined) continue;
 
 
-            if (this.collidesWith(gameObject, collideObject)) collidingObjects.push(collideObject);
+            if (this.collidesWith(gameObject, collideObject, zoneType)) collidingObjects.push(collideObject);
         }
 
         return collidingObjects;
     }
 
-    static #rectangularCZcollidesWithRectangularCZ(gameObject, collideObject){
-        let gameObjectCZfromX = gameObject.posX + gameObject.collideZone.fromX;
-        let gameObjectCZfromY = gameObject.posY + gameObject.collideZone.fromY;
-        let gameObjectCZuntilX = gameObject.posX + gameObject.collideZone.untilX;
-        let gameObjectCZuntilY = gameObject.posY + gameObject.collideZone.untilY;
+    static #rectangularCZcollidesWithRectangularCZ(gameObject, gameObjectZone, collideObject, collideObjectZone){
+        let gameObjectCZfromX = gameObject.posX + gameObjectZone.fromX;
+        let gameObjectCZfromY = gameObject.posY + gameObjectZone.fromY;
+        let gameObjectCZuntilX = gameObject.posX + gameObjectZone.untilX;
+        let gameObjectCZuntilY = gameObject.posY + gameObjectZone.untilY;
 
-        let collideObjectCZfromX = collideObject.posX + collideObject.collideZone.fromX;
-        let collideObjectCZfromY = collideObject.posY + collideObject.collideZone.fromY;
-        let collideObjectCZuntilX = collideObject.posX + collideObject.collideZone.untilX;
-        let collideObjectCZuntilY = collideObject.posY + collideObject.collideZone.untilY;
+        let collideObjectCZfromX = collideObject.posX + collideObjectZone.fromX;
+        let collideObjectCZfromY = collideObject.posY + collideObjectZone.fromY;
+        let collideObjectCZuntilX = collideObject.posX + collideObjectZone.untilX;
+        let collideObjectCZuntilY = collideObject.posY + collideObjectZone.untilY;
 
         return gameObjectCZfromX < collideObjectCZuntilX && gameObjectCZuntilX > collideObjectCZfromX &&
             gameObjectCZfromY < collideObjectCZuntilY && gameObjectCZuntilY > collideObjectCZfromY;
     }
 
-    static #circularCZcollidesWithRectangularCZ(gameObject, collideObject){
-        let czWidth = collideObject.collideZone.untilX - collideObject.collideZone.fromX;
-        let czHeight = collideObject.collideZone.untilY - collideObject.collideZone.fromY;
+    static #circularCZcollidesWithRectangularCZ(gameObject, gameObjectZone, collideObject, collideObjectZone){
+        let czWidth = collideObjectZone.untilX - collideObjectZone.fromX;
+        let czHeight = collideObjectZone.untilY - collideObjectZone.fromY;
         
-        let rectangleCenterX = collideObject.posX + collideObject.collideZone.fromX + czWidth/2;
-        let rectangleCenterY = collideObject.posY + collideObject.collideZone.fromY + czHeight/2;
+        let rectangleCenterX = collideObject.posX + collideObjectZone.fromX + czWidth/2;
+        let rectangleCenterY = collideObject.posY + collideObjectZone.fromY + czHeight/2;
 
         let circleDistanceX = Math.abs(gameObject.posX - rectangleCenterX);
         let circleDistanceY = Math.abs(gameObject.posY - rectangleCenterY);
 
-        if (circleDistanceX > (czWidth/2 + gameObject.collideZone.radius)) return false;
-        if (circleDistanceY > (czHeight/2 + gameObject.collideZone.radius)) return false;
+        if (circleDistanceX > (czWidth/2 + gameObjectZone.radius)) return false;
+        if (circleDistanceY > (czHeight/2 + gameObjectZone.radius)) return false;
 
         if (circleDistanceX <= (czWidth/2)) return true;
         if (circleDistanceY <= (czHeight/2)) return true;
@@ -74,6 +84,6 @@ class CollisionDetection{
         let cornerDistance_sq = (circleDistanceX - czWidth/2)^2 +
             (circleDistanceY - czHeight/2)^2;
 
-        return (cornerDistance_sq <= (gameObject.collideZone.radius^2));
+        return (cornerDistance_sq <= (gameObjectZone.radius^2));
     }
 }
--- scripts/rectangularHitZone.js
+++ scripts/rectangularHitZone.js
@@ -1,9 +1,5 @@
 class RectangularHitZone extends RectangularZone{
     constructor(fromX, fromY, untilX, untilY) {
-        super();
-        this.fromX = fromX;
-        this.fromY = fromY;
-        this.untilX = untilX;
-        this.untilY = untilY;
+        super(fromX, fromY, untilX, untilY);
     }
 }
