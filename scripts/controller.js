f9f5e7db8b5ac44aacfef9fa69ecfed4dc67b654
Philip <klattephilip@gmail.com>
2023-06-18 17:44:06 +0200

    refactor

--- scripts/controller.js
+++ scripts/controller.js
@@ -192,7 +192,8 @@ function draw() {
     effects.forEach(effect => effect.draw());
 
     drawKillCount();
-    showCollideZones();
+
+    //showCollideZones();
     //drawGrid(tilesize);
     //drawLineFromZombieToPlayer();
     //drawLineForWall();
