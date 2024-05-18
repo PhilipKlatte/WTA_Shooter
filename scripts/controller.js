06806b13f059632d9ba84669584069ad59d12069
PhilipKlatte <klattephilip@gmail.com>
2024-05-18 15:02:38 +0200

    test commit

--- scripts/controller.js
+++ scripts/controller.js
@@ -99,8 +99,6 @@ function init() {
     interval = setInterval(gameLoop,50);
 }
 
-// test
-
 function buildCanvas(){
     canvas = document.getElementById("canvas");
     canvas.setAttribute("width", (tilesX * tilesize).toString());
